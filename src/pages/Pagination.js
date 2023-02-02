86983774fd2e273806aab85eb21256b04af8e5a5
Sheikh sadi <sheikhsadi1620@gmail.com>
2023-02-03 00:17:28 +0600

    pagination updated

--- src/pages/Pagination.js
+++ src/pages/Pagination.js
@@ -1,14 +1,36 @@
 import React from 'react';
 
-const Pagination = ({totalPost}) => {
+const Pagination = ({ totalPost, setCurrentPage, currentPage }) => {
      let pages = []
 
-     for(let i = 1; i <= totalPost/3; i++){
+     for (let i = 1; i <= Math.ceil(totalPost / 3); i++) {
           pages.push(i)
      }
+
+     const handleBack = () =>{
+         currentPage > 1 ? setCurrentPage(currentPage - 1) : setCurrentPage(1)
+     }
+     const handleForward = () =>{
+          currentPage < 4 ? setCurrentPage(currentPage + 1) : setCurrentPage(4)
+     }
      return (
-          <div>
-               
+          <div  className='flex justify-center items-center'>
+               <div onClick={handleBack} className={`px-3 py-1 cursor-pointer ${currentPage === 1 && 'text-red-500'}`}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4  ">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+               </svg>
+
+               </div>
+               {
+                    pages.map((page, i) => {
+                         return <button key={i} onClick={() => setCurrentPage(page)} className={`px-3 py-1 ${page === currentPage && 'bg-red-500 text-white'}`}>{page}</button>
+                    })
+               }
+               <div onClick={handleForward} className={`px-3 py-1 cursor-pointer ${currentPage === 4 && 'text-red-500'}`}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                         <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                    </svg>
+
+               </div>
           </div>
      );
 };
--- src/pages/Users.js
+++ src/pages/Users.js
@@ -7,7 +7,7 @@ const Users = () => {
      const [open, setOpen] = useState(false)
      const [toggleState, setToggleState] = useState();
      const [currentPage, setCurrentPage] = useState(1);
-     
+
 
      const hanldeUser = (toggleId) => {
           setToggleState(toggleId)
@@ -27,13 +27,13 @@ const Users = () => {
      const firstPostIndex = lastPostIndex - 3;
      const currentPosts = users.slice(firstPostIndex, lastPostIndex)
 
-     if(isLoading){
+     if (isLoading) {
           return <div className='w-full h-screen flex justify-center items-center'>
                <Loading></Loading>
           </div>
      }
      return (
-          <div className='w-full bg-slate-300/30'>
+          <div className='w-full min-h-screen bg-slate-300/30'>
                <div className='mx-2 sm:mx-6 md:mx-12 lg:max-w-5xl lg:mx-auto py-5'>
                     {
                          currentPosts.map(user => <div key={user.id} className='my-8 py-5 md:py-8 px-3 md:px-8 bg-white rounded-md shadow-sm'>
@@ -55,41 +55,44 @@ const Users = () => {
 
                               </div>
                               <div className={toggleState === user.id && open ? 'block' : 'hidden'}>
-                                   
-                                        <div className='border mt-5 rounded-md w-full p-5 md:px-12 md:py-8'>
-                                             <p className='font-semibold text-sm'>Description</p>
-                                             <p className='text-sm mb-4'>{user.company.name + ' company owner'} </p>
-                                             <div className=' grid grid-cols-1 md:grid-cols-2'>
-                                                  <div>
-                                                       <p className='font-semibold text-sm'>Contact Person</p>
-                                                       <p className='text-sm mb-2'>{user.name}</p>
-                                                       <p className='font-semibold text-sm'>Username</p>
-                                                       <p className='text-sm mb-2'>{user.username}</p>
-                                                       <p className='font-semibold text-sm'>Emails</p>
-                                                       <p className='text-sm mb-2'>{user.email}</p>
-                                                       <p className='font-semibold text-sm'>Phones</p>
-                                                       <p className='text-sm mb-2'>{user.phone}</p>
-                                                  </div>
-                                                  <div>
-                                                       <p className='font-semibold text-sm'>Street</p>
-                                                       <p className='text-sm mb-2'>{user.address.street}</p>
-                                                       <p className='font-semibold text-sm'>City</p>
-                                                       <p className='text-sm mb-2'>{user.address?.city}</p>
-                                                       <p className='font-semibold text-sm'>Zip code</p>
-                                                       <p className='text-sm mb-2'>{user.address.zipcode}</p>
-                                                       <p className='font-semibold text-sm'>Website</p>
-                                                       <p className='text-sm mb-2'>{user?.website}</p>
-                                                  </div>
+
+                                   <div className='border mt-5 rounded-md w-full p-5 md:px-12 md:py-8'>
+                                        <p className='font-semibold text-sm'>Description</p>
+                                        <p className='text-sm mb-4'>{user.company.name + ' company owner'} </p>
+                                        <div className=' grid grid-cols-1 md:grid-cols-2'>
+                                             <div>
+                                                  <p className='font-semibold text-sm'>Contact Person</p>
+                                                  <p className='text-sm mb-2'>{user.name}</p>
+                                                  <p className='font-semibold text-sm'>Username</p>
+                                                  <p className='text-sm mb-2'>{user.username}</p>
+                                                  <p className='font-semibold text-sm'>Emails</p>
+                                                  <p className='text-sm mb-2'>{user.email}</p>
+                                                  <p className='font-semibold text-sm'>Phones</p>
+                                                  <p className='text-sm mb-2'>{user.phone}</p>
+                                             </div>
+                                             <div>
+                                                  <p className='font-semibold text-sm'>Street</p>
+                                                  <p className='text-sm mb-2'>{user.address.street}</p>
+                                                  <p className='font-semibold text-sm'>City</p>
+                                                  <p className='text-sm mb-2'>{user.address?.city}</p>
+                                                  <p className='font-semibold text-sm'>Zip code</p>
+                                                  <p className='text-sm mb-2'>{user.address.zipcode}</p>
+                                                  <p className='font-semibold text-sm'>Website</p>
+                                                  <p className='text-sm mb-2'>{user?.website}</p>
                                              </div>
                                         </div>
-                                   
+                                   </div>
+
                               </div>
                          </div>)
                     }
-                    <Pagination 
-                         totalPost={users.length}
-
-                    />
+                    
+                         <Pagination
+                              totalPost={users.length}
+                              setCurrentPage={setCurrentPage}
+                              currentPage={currentPage}
+                         />
+                   
 
                </div>
           </div>
